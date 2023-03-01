afb5fc28fbd5dae406dddc8763d85d4530775ec4
Dominik Płachta <dominik.plachta@8lines.io>
2023-03-01 21:23:50 +0100

    Move checking channel to controller

--- src/Controller/CartLinkController.php
+++ src/Controller/CartLinkController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace EightLines\SyliusCartLinksPlugin\Controller;
 
 use EightLines\SyliusCartLinksPlugin\Processor\CartLinkProcessorInterface;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Order\Context\CartContextInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +18,7 @@ final class CartLinkController extends AbstractController
         private CartLinkProcessorInterface $cartLinkProcessor,
         private RepositoryInterface $cartLinkRepository,
         private CartContextInterface $cartContext,
+        private ChannelContextInterface $channelContext,
     ) { }
 
     public function __invoke(string $code): Response
@@ -25,6 +27,10 @@ final class CartLinkController extends AbstractController
 
         if (null === $cartLink) {
             throw new NotFoundHttpException('Given cart link does not exist.');
+        }
+
+        if (false === in_array($this->channelContext->getChannel(), $cartLink->getChannels()->toArray(), true)) {
+            throw new NotFoundHttpException('Given cartLink is not available in this channel.');
         }
 
         $cart = $this->cartContext->getCart();
--- src/Processor/CartLinkProcessor.php
+++ src/Processor/CartLinkProcessor.php
@@ -6,23 +6,16 @@ namespace EightLines\SyliusCartLinksPlugin\Processor;
 
 use EightLines\SyliusCartLinksPlugin\Applicator\CartLinkActionApplicatorInterface;
 use EightLines\SyliusCartLinksPlugin\Entity\CartLinkInterface;
-use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Core\Model\OrderInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class CartLinkProcessor implements CartLinkProcessorInterface
 {
     public function __construct(
         private CartLinkActionApplicatorInterface $cartLinkActionApplicator,
-        private ChannelContextInterface $channelContext,
     ) { }
 
     public function process(OrderInterface $order, CartLinkInterface $cartLink): void
     {
-        if (false === in_array($this->channelContext->getChannel(), $cartLink->getChannels()->toArray(), true)) {
-            throw new NotFoundHttpException('Given cartLink is not available in this channel.');
-        }
-
         $this->cartLinkActionApplicator->apply($order, $cartLink);
     }
 }
