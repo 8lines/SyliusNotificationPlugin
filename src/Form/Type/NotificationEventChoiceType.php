2f41cc430959ec30d30522cf6cee4f44a338a6de
Dominik Płachta <dplachta03@gmail.com>
2023-12-12 17:46:29 +0100

    Make event choices are based on NotificationEvent ServiceRegistry

--- src/Form/Type/NotificationEventChoiceType.php
+++ src/Form/Type/NotificationEventChoiceType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace EightLines\SyliusNotificationPlugin\Form\Type;
 
+use Sylius\Component\Registry\ServiceRegistryInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,14 +12,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 final class NotificationEventChoiceType extends AbstractType
 {
     public function __construct(
-        private array $events,
+        private ServiceRegistryInterface $notificationEventsRegistry,
     ) {
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'choices' => array_flip($this->events),
+            'choices' => array_keys($this->notificationEventsRegistry->all()),
+            'choice_label' => fn (string $choice) => $choice,
         ]);
     }
 
