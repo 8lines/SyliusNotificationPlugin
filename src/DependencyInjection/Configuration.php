8510d99d9cbd15fbd99f67af9694f676639cddb0
Dominik Płachta <dplachta03@gmail.com>
2023-12-12 17:08:11 +0100

    Add initial migration of SyliusNotificationPlugin

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('eightlines_notification');
+        $treeBuilder = new TreeBuilder('eightlines_sylius_notification_plugin');
         $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
--- src/DependencyInjection/EightLinesSyliusNotificationExtension.php
+++ src/DependencyInjection/EightLinesSyliusNotificationExtension.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace EightLines\SyliusNotificationPlugin\DependencyInjection;
 
+use Sylius\Bundle\CoreBundle\DependencyInjection\PrependDoctrineMigrationsTrait;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class EightLinesSyliusNotificationExtension extends Extension
+final class EightLinesSyliusNotificationExtension extends Extension implements PrependExtensionInterface
 {
+    use PrependDoctrineMigrationsTrait;
+
     /**
      * @psalm-suppress UnusedVariable
      */
@@ -26,5 +30,25 @@ final class EightLinesSyliusNotificationExtension extends Extension
     public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
     {
         return new Configuration();
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->prependDoctrineMigrations($container);
+    }
+
+    protected function getMigrationsNamespace(): string
+    {
+        return 'EightLines\SyliusNotificationPlugin\Migrations';
+    }
+
+    protected function getMigrationsDirectory(): string
+    {
+        return '@EightLinesSyliusNotificationPlugin/migrations';
+    }
+
+    protected function getNamespacesOfMigrationsExecutedBefore(): array
+    {
+        return ['Sylius\Bundle\CoreBundle\Migrations'];
     }
 }
