aba52e07b6eab96d6a2df190e3b8e0762e020e41
Dominik Płachta <dplachta03@gmail.com>
2024-01-04 17:06:29 +0100

    Update OrderPaidNotificationEvent

--- src/NotificationEvent/Sylius/OrderPaidNotificationEvent.php
+++ src/NotificationEvent/Sylius/OrderPaidNotificationEvent.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace EightLines\SyliusNotificationPlugin\NotificationEvent\Sylius;
 
+use EightLines\SyliusNotificationPlugin\Form\Type\NotificationEvent\OrderPaidNotificationEventActionType;
 use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventInterface;
 use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariable;
+use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableDefinition;
+use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableDescription;
 use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableName;
-use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableNames;
+use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableDefinitions;
 use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariables;
 use EightLines\SyliusNotificationPlugin\NotificationEvent\NotificationEventVariableValue;
 use Sylius\Component\Core\Model\PaymentInterface;
@@ -17,6 +20,11 @@ class OrderPaidNotificationEvent implements NotificationEventInterface
     public static function getEventName(): string
     {
         return 'sylius.payment.post_complete';
+    }
+
+    public static function getConfigurationFormType(): ?string
+    {
+        return OrderPaidNotificationEventActionType::class;
     }
 
     public function getVariables(object $subject): NotificationEventVariables
@@ -43,11 +51,19 @@ class OrderPaidNotificationEvent implements NotificationEventInterface
         );
     }
 
-    public function getVariableNames(): NotificationEventVariableNames
+    public function getVariableDefinitions(): NotificationEventVariableDefinitions
     {
-        return NotificationEventVariableNames::create(
-            new NotificationEventVariableName('order_number'),
-            new NotificationEventVariableName('order_total'),
+        return NotificationEventVariableDefinitions::create(
+            new NotificationEventVariableDefinition(
+                name: new NotificationEventVariableName('order_number'),
+                defaultValue: new NotificationEventVariableValue(''),
+                description: new NotificationEventVariableDescription('Order number'),
+            ),
+            new NotificationEventVariableDefinition(
+                name: new NotificationEventVariableName('order_total'),
+                defaultValue: new NotificationEventVariableValue(''),
+                description: new NotificationEventVariableDescription('Order total'),
+            )
         );
     }
 }
