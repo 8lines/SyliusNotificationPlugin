009c8c78d7c8ed09c68e69e468abd598e783f198
Dominik Płachta <dplachta03@gmail.com>
2024-01-04 17:08:38 +0100

    Remove cart-link-actions.js

--- tests/Application/assets/admin/cart-link-actions.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-
-$(() => {
-  $('#actions a[data-form-collection="add"]').on('click', () => {
-    setTimeout(() => {
-      $('select[name^="cart_link[actions]"][name$="[type]"]').last().change();
-    }, 50);
-  });
-});
