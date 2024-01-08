1eb1dc7566f13b87c4b67b09169976e9b9bc951d
Dominik Płachta <dplachta03@gmail.com>
2024-01-08 19:33:14 +0100

    Update SlackNotificationChannel

--- src/NotificationChannel/Symfony/SlackNotificationChannel.php
+++ src/NotificationChannel/Symfony/SlackNotificationChannel.php
@@ -5,20 +5,25 @@ declare(strict_types=1);
 namespace EightLines\SyliusNotificationPlugin\NotificationChannel\Symfony;
 
 use EightLines\SyliusNotificationPlugin\NotificationChannel\NotificationChannelInterface;
+use EightLines\SyliusNotificationPlugin\NotificationChannel\NotificationContext;
 use Symfony\Component\Notifier\ChatterInterface;
+use Symfony\Component\Notifier\Exception\TransportExceptionInterface;
 use Symfony\Component\Notifier\Message\ChatMessage;
 
-class SlackNotificationChannel implements NotificationChannelInterface
+final class SlackNotificationChannel implements NotificationChannelInterface
 {
     public function __construct(
         private ChatterInterface $chatter,
     ) {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function send(
         string $message,
+        NotificationContext $context,
     ): void {
-
         $message = new ChatMessage($message);
         $message->transport('slack');
 
