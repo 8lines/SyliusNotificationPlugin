82b6a8dff8e94c07cf73919e168d73918397e2ee
Dominik Płachta <dplachta03@gmail.com>
2024-01-09 20:11:12 +0100

    Remove disabling eventCode field after notification creation

--- src/Form/Type/NotificationType.php
+++ src/Form/Type/NotificationType.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace EightLines\SyliusNotificationPlugin\Form\Type;
 
-use EightLines\SyliusNotificationPlugin\Entity\Notification;
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelChoiceType;
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
@@ -15,16 +14,11 @@ final class NotificationType extends AbstractResourceType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        /** @var Notification|null $notification */
-        $notification = $builder->getData();
-        $codeDisabled = null !== $notification?->getCode();
-
         $builder
             ->addEventSubscriber(new AddCodeFormSubscriber())
             ->add('eventCode', NotificationEventChoiceType::class, [
                 'label' => 'eightlines_sylius_notification_plugin.ui.event',
                 'required' => true,
-                'disabled' => $codeDisabled,
                 'attr' => [
                     'data-type' => 'notification-event-code',
                 ]
@@ -40,9 +34,6 @@ final class NotificationType extends AbstractResourceType
             ])
             ->add('actions', NotificationActionCollectionType::class, [
                 'label' => false,
-                'attr' => [
-                    'data-type' => 'notification-actions',
-                ],
             ])
         ;
     }
