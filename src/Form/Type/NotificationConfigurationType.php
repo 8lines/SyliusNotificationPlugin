20f4a51f6079c2f7b141882107a178d011d26746
Dominik Płachta <dplachta03@gmail.com>
2024-01-15 16:40:12 +0100

    Update Form Types (NotificationConfiguration & NotificationContent)

--- src/Form/Type/NotificationConfigurationType.php
+++ src/Form/Type/NotificationConfigurationType.php
@@ -6,18 +6,10 @@ namespace EightLines\SyliusNotificationPlugin\Form\Type;
 
 use EightLines\SyliusNotificationPlugin\Entity\NotificationConfiguration;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class NotificationConfigurationType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder->add('data', null, [
-            'label' => false,
-        ]);
-    }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
